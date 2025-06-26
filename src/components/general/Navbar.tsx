b2bc7a1dca3a10c1007cd7ed15c2ed7d3b97ffc3
siddhant-uniyal <siddhantuniyal416@gmail.com>
2025-06-26 21:41:32 +0530

    fix: fetch session on client side

--- src/components/general/Navbar.tsx
+++ src/components/general/Navbar.tsx
@@ -1,12 +1,14 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
 import { buttonVariants } from '../ui/button'
 import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+// import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
 
-const Navbar = async () => {
-  const { getUser } = getKindeServerSession()
-  const user = await getUser()
+const Navbar =  () => {
+  const { getUser } = useKindeBrowserClient()
+  const user = getUser()
   return (
     <nav className="py-5 flex items-center justify-between">
       <div className='flex items-center gap-6'>
